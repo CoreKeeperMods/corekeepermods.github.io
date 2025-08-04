d12da9a97a3690df168e3795f4eeb133f9b1ffaf
Minepatcher <theminepatcher@gmail.com>
2025-08-04 17:05:57 -0600

    update 3.x documentation
    - customized some plugins
    - will work on 4.x next

---
+++ js/docsify-dark-switcher-plugin.js
@@ -0,0 +1,47 @@
+﻿/*
+* Edited docsify-dark-switcher
+* https://github.com/w3teal/docsify-dark-switcher
+* */
+
+function docsifyDarkSwitcher() {
+    var element = document.querySelector('body');
+    var btn = document.querySelector('button.docsify-dark-switcher');
+    element.classList.toggle("docsify-dark-mode");
+    
+    if (element.classList.contains("docsify-dark-mode")) {
+        localStorage.setItem('docsify-dark-mode', 'true');
+        btn.title = "Toggle light mode";
+        btn.ariaLabel = "Toggle light mode";
+    } else {
+        localStorage.removeItem('docsify-dark-mode');
+        btn.title = "Toggle dark mode";
+        btn.ariaLabel = "Toggle dark mode";
+    }
+}
+
+function applyInitialMode() {
+    var element = document.querySelector('body');
+    var btn = document.querySelector('button.docsify-dark-switcher');
+    if (localStorage.getItem('docsify-dark-mode') === 'true' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        element.classList.add("docsify-dark-mode");
+        btn.title = "Toggle light mode";
+        btn.ariaLabel = "Toggle light mode";
+    } else {
+        btn.title = "Toggle dark mode";
+        btn.ariaLabel = "Toggle dark mode";
+    }
+}
+
+var install = function (hook, vm) {
+    hook.mounted(function() {
+        var themeSwitcherBtn = document.createElement('button');
+        themeSwitcherBtn.id = "docsify-dark-switcher";
+        themeSwitcherBtn.className = 'docsify-dark-switcher';
+        themeSwitcherBtn.onclick = docsifyDarkSwitcher;
+        var main = document.querySelector('.sidebar');
+        main.insertBefore(themeSwitcherBtn, main.firstChild);
+        applyInitialMode();
+    });
+}
+
+$docsify.plugins = [].concat(install, $docsify.plugins);
